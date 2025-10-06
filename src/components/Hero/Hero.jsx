20be7c0168928faf456315f9161f28aa7c9f6ddf
Christian Balidoy <redchristian20@gmail.com>
2025-10-06 14:49:43 +0800

    .

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -66,7 +66,7 @@ const Hero = () => {
                 <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-screen-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-4">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/irrigation-stock.jpg" alt=""/>
                     <div class="flex flex-col justify-between p-4 leading-normal">
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Our models are trained on 10+ years of regional data, achieving over 909% accuracy in trend forecasting for key highland vegetables</p>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Our models are trained on 10+ years of regional data, achieving over 90% accuracy in trend forecasting for key highland vegetables</p>
                     </div>
                 </a>
 
